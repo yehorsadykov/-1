b522517bb9dbc9cb478b942579b91a4563416467
Yehor Sadykov <97795698+yehorsadykov@users.noreply.github.com>
2024-09-06 18:41:47 +0300

    Add files via upload

---
+++ Приклад1/Приклад1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string s = "Dfg+t5-r7y;asd*s34;rt";
+
+int t = s.IndexOf(";");
+int i, k = 0;
+
+for (i = 0; i <= t; i++)
+{
+    if (char.IsLetter(s[i]))
+    {
+        k++;
+    }
+}
+
+Console.WriteLine(k);
+Console.ReadKey();
