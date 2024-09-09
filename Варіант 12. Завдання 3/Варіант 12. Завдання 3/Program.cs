35de6bd3c5f730dfc53f49339ef3000c18405a87
Yehor Sadykov <97795698+yehorsadykov@users.noreply.github.com>
2024-09-09 16:06:53 +0300

    Add files via upload

---
+++ Варіант 12. Завдання 3/Варіант 12. Завдання 3/Program.cs
@@ -0,0 +1,33 @@
+﻿
+string s = "apple banana avocado"; // оголошуємо змінну з рядком
+string[] words = s.Split(" "); // розбиваємо рядок на слова
+
+// проходимо по кожному слову в реченні за допомогою циклу for
+for (int i = 0; i < words.Length; i++)
+{
+    string word = words[i]; // беремо поточне слово
+
+    // перевіряємо, чи слово не порожнє
+    if (word.Length > 0)
+    {
+        char firstLetter = word[0]; // беремо першу букву слова
+        
+        int count = 0; // рахуємо кількість входжень першої букви в слові
+
+        for (int j = 0; j < word.Length; j++)
+        {
+            if (word[j] == firstLetter)
+            {
+                count++; // збільшуємо лічильник, якщо буква співпадає з першою
+            }
+        }
+
+        // якщо перша буква зустрічається більше одного разу, виводимо слово
+        if (count > 1)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
+
+Console.ReadKey();
---
+++ Варіант 12. Завдання 4/Варіант 12. Завдання 4/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string s = "мама миє посуд"; // оголошуємо рядок з реченням
+string[] words = s.Split(' '); // розбиваємо речення на слова
+string newS = ""; // оголошуємо змінну для зберігання нового речення
+
+// проходимо по кожному слову через цикл for
+for (int i = 0; i < words.Length; i++)
+{
+    // якщо номер слова непарний (i + 1, бо індекси починаються з 0)
+    if ((i + 1) % 2 != 0)
+    {
+        string word = words[i]; // беремо слово та виводимо його у зворотному порядку
+        string reversedWord = ""; // перевертаємо слово
+
+        for (int j = word.Length - 1; j >= 0; j--)
+        {
+            reversedWord += word[j];
+        }
+
+        char firstLetter = char.ToUpper(reversedWord[0]); // перша буква буде велика
+        string formattedWord = firstLetter + reversedWord.Substring(1).ToLower(); // останні букви будуть з малої літери
+        newS += formattedWord + " "; // додаємо перевернуте слово до нового речення
+    }
+}
+
+Console.WriteLine(newS.Trim()); // виводимо нове речення, видалаючі пробіли в кінці
+Console.ReadKey(); 
