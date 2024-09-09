475fed89587f067fad465795264800c1460e20d3
Yehor Sadykov <97795698+yehorsadykov@users.noreply.github.com>
2024-09-09 11:18:40 +0300

    Add files via upload

---
+++ Приклад 2/Приклад 2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string s = "АУАУАПАУК";
+int i = 0;
+
+do
+{
+    string ss = s.Substring(i, 2);
+
+    if (ss == "АУ")
+    {
+        s = s.Insert(i, "О");
+        i = i + 3;
+    }
+    else
+    {
+        i++;
+    }
+}
+
+while (i < s.Length - 1); 
+
+Console.WriteLine(s);
+Console.ReadKey();
