66eb7056ecbf5c787709efe347a8ff0bf5afe61a
Yehor Sadykov <97795698+yehorsadykov@users.noreply.github.com>
2024-09-09 11:25:16 +0300

    Add files via upload

---
+++ Приклад 3/Приклад 3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string s = "біологія алгебра історія географія геометрія";
+string c = "і";
+string[] a;
+
+a = s.Split(" ");
+
+int i;
+
+for (i = 0; i < a.Length; i++)
+{
+    int t = a[i].IndexOf(c);
+
+    if (t != -1)
+    {
+        Console.WriteLine(a[i]);
+    }
+}
+
+Console.ReadKey(); 
+
+/* біологія
+історія
+географія
+геометрія */
